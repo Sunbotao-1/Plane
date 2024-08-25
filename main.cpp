34852250c7a627a525270e96c9784db1dc82d881
Sunbotao-1 <1955916253@qq.com>
2024-08-25 19:59:23 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include"page_login.h"
 #include <QApplication>
-
+#include"citysql.h"
 int main(int argc, char *argv[])
 {
    QApplication a(argc, argv);
    MainWindow w;
+
+   citySql sql;
 
     return a.exec();
 }
