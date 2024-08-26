1b47f8c1a18b887d184eb439796cfd9ef1befd88
Sunbotao-1 <1955916253@qq.com>
2024-08-26 14:22:34 +0800

    Add files via upload
    数据库的初步构建已完成

--- citysql.cpp
+++ citysql.cpp
@@ -10,6 +10,34 @@ citySql::citySql(QObject *parent)
 
     init();
 
+    // CityInfo c;
+    // c.name="北京";
+    // for(int i=0;i<10;i++)
+    // {
+    //         addCity(c);
+    // }
+    // getCityCnt();
+    // getPageCity(2,3);
+    //delCity(1003);
+    // CityInfo c;
+    // c.id=1004;
+    // c.name="上海";
+    // UpdateCityInfo(c);
+
+
+
+       // UserInfo info;
+       // info.username = "xiaoyu";
+       // info.password = "123456";
+       // info.aut = "admin";
+
+       // addUser(info);
+       // addUser(info);
+       // auto l=getAllUser();
+       // qDebug()<<isExist("xiaoyu");
+       // info.password = "666";
+       // updateUser(info);
+       //delUser("xiaoyu");
 }
 
 void citySql::init()//打开数据库???
@@ -17,23 +45,138 @@ void citySql::init()//打开数据库???
     if(QSqlDatabase::drivers().isEmpty())
         qDebug()<<"No database drivers found";
 
-   m_db =QSqlDatabase::addDatabase("QSQLITE");//打开数据库????
-   //auto str=QCoreApplication::applicationDirPath();
-  // qDebug()<<str;//应用程序的输出目录.
+    m_db =QSqlDatabase::addDatabase("QSQLITE");//打开数据库????
+//#if
+   // auto str=QCoreApplication::applicationDirPath()+"data.db";
+         //qDebug()<<str;//应用程序的输出目录.
+//#endif
+
 
 
     m_db.setDatabaseName("D:\\data.db");
     if(!m_db.open())
         qDebug()<<"db not open";
 }
-//quint32 citySql::getcity()
-//{
+quint32 citySql::getCityCnt()
+{
+    QSqlQuery sql(m_db);
+    sql.exec("select count(id) from city");
+    quint32 uiCnt=0;
+    while(sql.next())
+    {
+        uiCnt=sql.value(0).toUInt();
+    }
+    qDebug()<<uiCnt;
+    return uiCnt;
+}
 
-//}
+QList<CityInfo> citySql::getPageCity(quint32 page, quint32 uiCnt)
+{
+    QList<CityInfo> l;
 
-//bool citySql::addCity(CityInfo info)
-//{
-  //  QSqlQuery sql(m_db);
-    // sql.exec("INSERT INTO city VALUES (1000, '南京', 10, 3);");
+    QSqlQuery sql(m_db);
+    QString strsql=QString("select * from city order by id limit %1 offset %2;"
+                             ).arg(uiCnt).arg(page*uiCnt);
+    sql.exec(strsql);
+    CityInfo info;
+    while(sql.next())
+    {
+        info.id=sql.value(0).toUInt();
+        info.name=sql.value(1).toString();
+        info.PointX=sql.value(2).toDouble();
+        info.PointY=sql.value(3).toDouble();
 
-//}
+    }
+    return l;
+}
+
+bool citySql::addCity(CityInfo info)
+{
+  QSqlQuery sql(m_db);
+    QString strSql =QString("insert into city values(null,'%1',%2,%3)").
+                   arg(info.name).
+                   arg(info.PointX).
+                   arg(info.PointY);
+ return sql.exec(strSql);
+
+}
+
+bool citySql::delCity(int id)
+{
+
+    QSqlQuery sql(m_db);
+    return sql.exec(QString("delete from city where id = %1").arg(id));
+
+}
+
+void citySql::clearCityTable()
+{
+    QSqlQuery sql(m_db);
+    sql.exec("delete from city");
+}
+
+void citySql::UpdateCityInfo(CityInfo info)
+{
+    QSqlQuery sql(m_db);
+    QString strSql =QString("update city set name = '%1',PointX=%2,PointY=%3 where id=%4;").
+                     arg(info.name).
+                     arg(info.PointX).
+                     arg(info.PointY).arg(info.id);
+     sql.exec(strSql);
+}
+
+QList<UserInfo> citySql::getAllUser()
+{
+    QList<UserInfo> l;
+
+    QSqlQuery sql(m_db);
+    sql.exec("select * from username");
+    UserInfo info;
+    while(sql.next())
+    {
+        info.username=sql.value(0).toString();
+        info.password=sql.value(1).toString();
+        info.aut=sql.value(2).toString();
+        l.push_back(info);
+    }
+    return l;
+}
+
+bool citySql::isExist(QString strUser)
+{
+    QSqlQuery sql(m_db);
+    sql.exec(QString("select *from username where username='%1'").arg(strUser));
+    return sql.next();//判断是否存在???
+}
+
+bool citySql::updateUser(UserInfo info)
+{
+    QSqlQuery sql(m_db);
+    QString strSql= QString("update username set password='%1',auth='%2' where username='%3'").
+                     arg(info.password).
+                     arg(info.aut).arg(info.username);
+
+    bool ret =   sql.exec(strSql);
+    // QSqlError e =  sql.lastError();
+    // if(e.isValid())
+    // {
+    //     qDebug()<<e.text();
+    // }
+    return ret;
+}
+
+bool citySql::addUser(UserInfo info)
+{
+    QSqlQuery sql(m_db);
+    QString strSql= QString("insert into username  values ('%1', '%2', '%3')").
+                     arg(info.username).
+                     arg(info.password).
+                     arg(info.aut);
+    return sql.exec(strSql);
+}
+
+bool citySql::delUser(QString strUserName)
+{
+    QSqlQuery sql(m_db);
+   return sql.exec(QString("delete from username where username='%1'").arg(strUserName));
+}
--- citysql.h
+++ citysql.h
@@ -26,9 +26,9 @@ public:
 
     void init();//初始化
     //查询所有城市数量(同步接口)(主线程)
-    quint32 getcity();
-    //查询第几页城市数据
-    QList<CityInfo> getPageCity(quint32 page,quint32 uiCity);//参数一:页数,参数二:数量.
+    quint32 getCityCnt();
+    //查询第几页城市数据,页数从0开始
+    QList<CityInfo> getPageCity(quint32 page,quint32 uiCnt);//参数一:页数,参数二:数量.
     //增加城市
     bool addCity(CityInfo);
     //删除城市
@@ -40,14 +40,14 @@ public:
     //查询所有用户
     QList<UserInfo> getAllUser();
     //查询用户名是否存在
-    bool isExit(QString strUser);
-    //修改用户权限
-    void changeuserAut(QString str);
+    bool isExist(QString strUser);
+    //更新用户信息
+   bool updateUser(UserInfo info);
     //添加单个用户
-    void addUser(UserInfo info);
+    bool addUser(UserInfo info);
 
     //删除单个用户
-    void delUser(QString strUserName);
+    bool delUser(QString strUserName);
 signals:
 
 private:
